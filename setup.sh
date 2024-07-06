ced1efdb99f471b7d30d0a43216997685c360411
Ryan <leehahnwoong@gmail.com>
2024-07-06 01:21:32 -0700

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-# Update package list
-apt-get update && \
-    apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+# Update Package & Install Dependencies
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-# Create Docker GPG key storage directory
-mkdir -p /etc/apt/keyrings
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# Download Docker GPG key
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Add Docker Repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-# Add Docker repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Update Package & Install Docker
+sudo apt-get update
+sudo apt-get install -y docker-ce
 
-# Update package list again and install Docker and Docker Compose
-apt-get update && \
-    apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# Start Docker Service
+sudo systemctl start docker
+sudo systemctl enable docker
 
-# Add user to Docker and Jenkins groups
-usermod -aG docker $USER && \
-    usermod -aG users jenkins \
-    usermod -aG docker ubuntu \
-    usermod -aG docker jenkins
+# Current User Add to Docker Group
+sudo usermod -aG docker $USER
+
+# Installation of Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# 설치 확인
+docker --version
+docker-compose --version
+
+echo "Docker & Docker Compose Installation Complete."
 
 systemctl reboot
